926f9f661335f30d6bb0e9bcd81e13774348cdca
keremduman30 <berwari30_1907@hotmail.com>
2023-11-25 23:03:55 +0300

    improved offer part

--- src/component/Offer.tsx
+++ src/component/Offer.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
 import React from "react";
+import CountDown from "./CountDown";
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
+      <div className="flex-1 flex flex-col items-center justify-center text-center gap-8 p-6 ">
+        <h1 className="text-white text-5xl xl:text-6xl">
+          Delicious Burger & French Fry
+        </h1>
+        <p className="text-white xl:text-xl">
+          Progressively simplify effective e-toilers and process-centric methods
+          of empowerment. Quickly pontificate parallel.
+        </p>
+        <CountDown />
+        <button className="bg-red-500 text-white rounded-md py-3 px-6 hover:bg-red-600 hover:scale-110 w-[50%] md:[40%] lg:w-[30%]">
+          Order Now
+        </button>
+      </div>
+      <div className="flex-1 w-full relative md:h-full">
+        <Image src="/offerProduct.png" alt="" fill className="object-contain" />
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
