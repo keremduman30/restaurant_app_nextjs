b8baf824872bdd31ade44a8707d1589da9228f61
keremduman30 <berwari30_1907@hotmail.com>
2023-11-26 17:11:19 +0300

    single product page improved

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,36 @@
+import Price from "@/component/Price";
+import { singleProduct } from "@/data";
+import Image from "next/image";
 import React from "react";
 
 const SingleProduct = () => {
-  return <div>SingleProduct</div>;
+  return (
+    <div className="p-4 lg:px-20 xl:p-40 h-screen flex flex-col  justify-around text-red-500 md:flex-row md:items-center md:gap-8 ">
+      {/* image container */}
+      {singleProduct.img && (
+        <div className="relative w-full h-1/2 md:h-[70%] ">
+          <Image
+            src={singleProduct.img}
+            alt=""
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
+      {/* text container */}
+      <div className=" h-1/2 flex flex-col gap-5 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
+        <h1 className="text-3xl uppercase font-bold my-2 xl:text-5xl">
+          {singleProduct.title}
+        </h1>
+        <p>{singleProduct.desc}</p>
+        <Price
+          price={singleProduct.price}
+          id={singleProduct.id}
+          options={singleProduct.options}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
